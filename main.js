63e370ddd4a2be4206cf3965209acebbf3512949
Gustavo Vieira <Guxtaviko@hotmail.com>
2022-07-23 23:36:23 -0300

    Criação do DarkMode

--- main.js
+++ main.js
@@ -20,11 +20,93 @@ closeBtn.addEventListener('click', () => {
     openBtn.style.display = "inline-block";
 })
 
+// switch theme
+const getStyle = (element, style) => 
+    window
+    .getComputedStyle(element)
+    .getPropertyValue(style)
+
+
+const root = document.querySelector(':root');
+const switcher = document.querySelector(".theme__switch");
+const themeIcon = document.querySelector(".theme-icon");
+
+const initialColors = {
+    colorBg: getStyle(root, "--color-bg"),
+    colorLight: getStyle(root, "--color-light"),
+    colorWhite: getStyle(root, "--color-white"),
+    colorBlack: getStyle(root, "--color-black")
+}
+
+const darkMode = {
+    colorBg: "#212121",
+    colorLight: "#414141",
+    colorWhite: "#121212",
+    colorBlack: "#FFF"
+}
+
+const transformKey = key => "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key =>
+        root.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+const clearThemeIcon = () => {
+    themeIcon.className = "";
+    themeIcon.classList.add('uil', 'theme-icon');
+}
+
+let isDark = false;
+
+window.onload = () => {
+    if (localStorage.theme) {
+        clearThemeIcon();
+        themeIcon.classList.add(localStorage.icon);
+        var theme = localStorage.theme
+        switch (theme) {
+            case 'dark':
+                changeColors(darkMode);
+                isDark = true;
+                break;
+            case 'initial':
+                changeColors(initialColors);
+                isDark = false;
+                break;
+            default:
+                changeColors(initialColors);
+                break;
+        }
+    }
+}
+
+const changeTheme = () => {
+    isDark = !isDark;
+    clearThemeIcon();
+    if (isDark) {
+        themeIcon.classList.add('uil-moon');
+        changeColors(darkMode);
+        localStorage.setItem('icon', 'uil-moon');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeIcon.classList.add('uil-sun');
+        changeColors(initialColors);
+        localStorage.setItem('icon', 'uil-sun');
+        localStorage.setItem('theme', 'inital');
+    }
+}
+
+switcher.addEventListener('click', () => {
+    changeTheme();
+})
+
+
 // show/hide secret cmd 
 const cmd = document.querySelector(".secret__cmd");
 const secretBtn = document.getElementById("secret");
 const cmdText = document.getElementById("auto-type");
-const devs = document.querySelector(".secret__devs")
+const devs = document.querySelector(".secret__devs");
 
 var messageArray = [`C:\\Users\\3DS\\2022> <span class="cmd__cd">cd Desenvolvedores</span> \nC:\\Users\\3DS\\2022\\Desenvolvedores>`];
 var textPosition = 0;
@@ -215,10 +297,6 @@ let conselhoEscolar = [
 let tableData = equipeDocente
 
 let i = 0;
-
-window.onload = () => {
-    loadTableData(tableData)
-}
 
 function setTableData(data) {
     tableData = data;

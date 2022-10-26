78064b6640ea39d7eeb6c1ad024ccbfb1fb9f97f
Gustavo Vieira <66680914+Guxtaviko@users.noreply.github.com>
2022-10-26 12:10:58 -0300

    Adicionado renderização dinamica das noticias

--- src/controllers/web-controller.js
+++ src/controllers/web-controller.js
@@ -1,7 +1,36 @@
 const Sequelize = require('sequelize');
 const connection = require('../database/database')
 const User = require('../models/user')(connection, Sequelize)
+const Notice = require('../models/notice')(connection, Sequelize)
+const Category = require('../models/category')(connection, Sequelize)
+
+Notice.associate({Category})
+Category.associate({Notice})
+
 const bcrypt = require('bcrypt');
+
+const renderNews = async (req, res) => {
+    const news = await Notice.findAll({order: [['updatedAt', 'DESC']], include: {
+        model: Category,
+        attributes: ['category_name']
+    }})
+
+    const categories = await Category.findAll()
+
+    res.render('news', {
+        news: news,
+        categories: categories
+    })
+}
+
+const renderNotice = async (req, res) => {
+    let { title } = req.params
+    title = title.split('_').join(' ')
+
+    const notice = await Notice.findOne({where: {notice_title: title}, raw: true})
+
+    res.render('notice', {notice: notice})
+}
 
 const loginUser = async (req, res) => {
     const { username, password } = req.body
@@ -15,5 +44,7 @@ const loginUser = async (req, res) => {
 }
 
 module.exports = {
+    renderNews,
+    renderNotice,
     loginUser
 }
--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -5,8 +5,8 @@ const controller = require('../controllers/web-controller')
 
 router.get('/', (req, res) => res.render('index'));
 router.get('/cursos', (req, res) => res.render('courses'));
-router.get('/noticias', (req, res) => res.render('news'));
-router.get('/noticia/:title', (req, res) => res.render('notice'))
+router.get('/noticias', controller.renderNews);
+router.get('/noticia/:title', controller.renderNotice)
 router.get('/sobre', (req, res) => res.render('about'));
 router.get('/diretoria_academica', (req, res) => res.render('academicBoard'));
 router.get('/diretoria_de_servicos', (req, res) => res.render('servicesBoard'));
@@ -15,4 +15,4 @@ router.get('/contato', (req, res) => res.render('contact'));
 router.get('/login', (req, res) => res.render('login', { error : ''}));
 router.post('/login', controller.loginUser);
 
-module.exports = router;
+module.exports = router; 
